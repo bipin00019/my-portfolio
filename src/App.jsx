90ef7c79f527e04945bc6c4b490df827c12d0df1
Bipin Tamang <bipintamang00019@gmail.com>
2026-05-22 14:53:35 +0545

    Updated About Page

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import Navbar from './components/Navbar/navbar'
 import Home from './components/Pages/Home'
+import About from './components/About/about'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -8,9 +9,16 @@ function App() {
   return (
     <>
       <Navbar />
-      <Home />
+      <section id="home">
+        <Home />
+      </section>
+
+      <section id="about">
+        <About />
+      </section>
     </>
   )
 }
 
 export default App
+
